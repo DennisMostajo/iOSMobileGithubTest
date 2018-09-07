41544cb391b47a65ffa3b24cda69448e097ab328
Dennis Mostajo <mostygt@gmail.com>
2018-09-07 11:30:41 -0400

    Initial commit: - create iOS Project for Mobile GitHub Users App - adding libraries with cocoapods

---
+++ iOSMobileGithubTest/iOSMobileGithubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSMobileGithubTest
+//
+//  Created by Dennis Mostajo on 7/9/18.
+//  Copyright © 2018 Dennis Mostajo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
